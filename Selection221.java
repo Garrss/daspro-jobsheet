bcf2312dfdb5a8c8589191139f8cfe18f9e32149
TyoPrasetyo <tyo190066@bitbucket.bri.co.id>
2023-09-29 14:35:25 +0700

    commit week 5

--- Selection221.java
+++ Selection221.java
@@ -19,6 +19,25 @@ public class Selection221 {
 
         System.out.println("Final grade = " + total + " and the decission " + message);
 
+        String predikat = "";
+        if(total > 80 && total < 100){
+            predikat = "A";
+        }else if(total > 73 && total < 80){
+            predikat = "B+";
+        }else if(total > 65 && total < 73){
+            predikat = "B";
+        }else if(total > 60 && total < 65){
+            predikat = "C+";
+        }else if(total > 50 && total < 60){
+            predikat = "C";
+        }else if(total > 39 && total < 50){
+            predikat = "D";
+        }else {
+            predikat = "E";
+        }
+        System.out.println("Predikat Nilai Anda : "+predikat);
+        
+        }
     }
 
-}
+

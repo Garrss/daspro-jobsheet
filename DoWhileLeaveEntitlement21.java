d7da66ef1954ade8f16bbc26fd3b4b112867a807
TyoPrasetyo <tyo190066@bitbucket.bri.co.id>
2023-10-13 15:11:28 +0700

    week 7

---
+++ DoWhileLeaveEntitlement21.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class DoWhileLeaveEntitlement21 {
+    public static void main(String[] args) {
+        Scanner input21 = new Scanner(System.in);
+        int jatahcuti, jumlahhari;
+        String konfirmasi;
+
+        System.out.print("Jatah cuti : ");
+        jatahcuti = input21.nextInt();
+
+        do{
+            System.out.print("Apakah Anda Ingin mengambil cuti (y/t)? ");
+            konfirmasi = input21.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")){
+                System.out.print("jumlah hari: ");
+                jumlahhari = input21.nextInt();
+
+                if (jumlahhari <= jatahcuti) {
+                    jatahcuti -= jumlahhari;
+                    System.out.println("Sisa jatah cuti: " + jatahcuti);
+                } else {
+                System.out.println("Sisa jatah cuti anda tidak mencukupi ");
+                break;
+                }
+            }
+        } while (jatahcuti > 0);
+    }
+}

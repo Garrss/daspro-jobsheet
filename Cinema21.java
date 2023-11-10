9ee6d0c7d7b46baff2e8bd711396262ef6501581
Muhammad Tegar Hibatulloh <143469879+Garrss@users.noreply.github.com>
2023-11-10 15:29:11 +0700

    week 10 bagian 2

--- Cinema21.java
+++ Cinema21.java
@@ -18,10 +18,10 @@ public class Cinema21 {
         System.out.printf("%s \t %s\n", audience[2][0], audience[2][1]);
         System.out.printf("%s \t %s\n", audience[3][0], audience[3][1]);
 
-        System.out.println(audience[0].length);
-        System.out.println(audience[1].length);
-        System.out.println(audience[2].length);
-        System.out.println(audience[3].length);
+        System.out.println(audience.length);
+        for (int i = 0; i < audience.length; i++) {
+            System.out.println("Length of row " + (i + 1) + ": " + audience[i].length);
+        }
 
     }
 }

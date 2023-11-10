3bdd39bcaadb1d048e3a2635eafc26b3ca6dc9b8
Muhammad Tegar Hibatulloh <143469879+Garrss@users.noreply.github.com>
2023-11-10 14:31:36 +0700

    week 10

--- Cinema21.java
+++ Cinema21.java
@@ -22,8 +22,8 @@ public class Cinema21 {
             System.out.println("Length of row: " + rowAudience.length);
         
         System.out.println("Audience in the row 3: ");
-        for (int i = 0; i < audience[2].length;i++) {
-            System.out.println(audience[2][i]);
+        for (String i : audience[2]) {
+            System.out.println(i);
         }
         }
     }

cc6925b1eb96c91b2c9987b884f60012ced0afdf
Muhammad Tegar Hibatulloh <143469879+Garrss@users.noreply.github.com>
2023-11-12 12:37:32 +0700

    percobaan 2 untuk fitur 2

--- CinemaWithScanner21.java
+++ CinemaWithScanner21.java
@@ -3,26 +3,47 @@ import java.util.Scanner;
 public class CinemaWithScanner21 {
     public static void main(String[] args) {
         Scanner input21 = new Scanner(System.in);
+
         int row, column;
         String name, next;
+        int menuChoice;
 
         String[][] audience = new String[4][2];
 
         while (true) {
-            System.out.print("Enter a name: ");
-            name = input21.nextLine();
-            System.out.print("Enter row number: ");
-            row = input21.nextInt();
-            System.out.print("Enter column number: ");
-            column = input21.nextInt();
+            System.out.println("Menu Options: ");
+            System.out.println("1. Input audience data");
+            System.out.println("2. Show audience list");
+            System.out.println("3. Exit");
+            System.out.println("Enter your choice: ");
+            menuChoice = input21.nextInt();
             input21.nextLine();
+            switch (menuChoice) {
+                case 1:
+                    System.out.println("Enter a name: ");
+                    name = input21.nextLine();
+                    System.out.println("Enter a row number: ");
+                    row = input21.nextInt();
+                    System.out.println("Enter a column number: ");
+                    column = input21.nextInt();
+                    input21.nextLine();
 
-            audience[row - 1][column - 1] = name;
-            System.out.print("Are there any other audiences to be added? (y/n): ");
-            next = input21.nextLine();
-
-            if (next.equalsIgnoreCase("n")) {
-                break;
+                    audience[row - 1][column - 1] = name;
+                    break;
+                case 2:
+                    System.out.println("Audience List: ");
+                    for (int i = 0; i < audience.length; i++) {
+                        for (int j = 0; j < audience[i].length; j++) {
+                            System.out.print(audience[i][j] + " ");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
             }
         }
     }

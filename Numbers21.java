7b8d953abfccd1957723e58f328aa74e5c59a2f7
Muhammad Tegar Hibatulloh <143469879+Garrss@users.noreply.github.com>
2023-11-14 19:52:35 +0700

    experiment 3 week 10 perubahan 2

--- Numbers21.java
+++ Numbers21.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class Numbers21 {
     public static void main(String[] args) {
 
-        int[][] mynumbers = new int[3][];
+        int[][] mynumbers = new int[3][5];
         mynumbers[0] = new int[5];
         mynumbers[1] = new int[3];
         mynumbers[2] = new int[1];
 
         for (int i = 0; i < mynumbers.length; i++) {
-            System.out.println(Arrays.toString(mynumbers[i]));
+            System.out.println("Length of row " + (i + 1) + ": " + mynumbers[i].length);
         }
     }
 }

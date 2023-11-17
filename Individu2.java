6275ea0e176142ccd4d41181a557c5a7550ac180
Muhammad Tegar Hibatulloh <143469879+Garrss@users.noreply.github.com>
2023-11-17 08:33:57 +0700

    individu

---
+++ Individu2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Individu2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan angka : ");
+        int number = sc.nextInt();
+
+        for (int i = 0; i < number; i++) {
+            for (int j = number; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

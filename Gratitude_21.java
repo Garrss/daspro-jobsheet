a40dfd5caee8ea73f86152fc564222e0f464380b
Muhammad Tegar Hibatulloh <143469879+Garrss@users.noreply.github.com>
2023-11-24 13:45:44 +0700

    Jobsheet 13

--- Gratitude_21.java
+++ Gratitude_21.java
@@ -1,7 +1,16 @@
 public class Gratitude_21 {
-    public static void main(String[] args) {
+    public static void sayThankyou() {
         System.out.println("Thank you for being the best teacher in the world.\n"
                 + "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
 
+    public static void sayAdditionalGreetings(String greeting) {
+        System.out.println(greeting);
+    }
+
+    public static void main(String[] args) {
+        sayThankyou();
+        String expresion = "Thankyou....wish you all the best!!";
+        sayAdditionalGreetings(expresion);
     }
 }

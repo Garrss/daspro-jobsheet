ca62867d5b63f41fb14556f426ec90e742b58520
Muhammad Tegar Hibatulloh <143469879+Garrss@users.noreply.github.com>
2023-11-24 15:38:09 +0700

    Jobsheet 13

--- ExpressingGratitude_21.java
+++ ExpressingGratitude_21.java
@@ -1,21 +1,32 @@
 import java.util.Scanner;
 
 public class ExpressingGratitude_21 {
+    private static Scanner input = new Scanner(System.in);
+
     public static String getGreetingRecipient() {
-        Scanner input = new Scanner(System.in);
         System.out.print("Input the name of people you want to greet : ");
         String recipientName = input.nextLine();
-        input.close();
         return recipientName;
     }
 
     public static void sayThankyou() {
         String name = getGreetingRecipient();
-        System.out.println("Thank you " + name + " for being the best teacher in the world.\n"
-                + "You inspired in me a love for learning and made me feel like I could ask you anything.");
+        System.out.println("Thank you " + name + " for being the best teacher in the world.");
+        System.out.println("You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+
+    public static void sayAdditionalGreetings(String greeting) {
+        System.out.println(greeting);
     }
 
     public static void main(String[] args) {
+        // String name = getGreetingRecipient();
         sayThankyou();
+
+        String addgreeting = "Wish you all the best for your future!";
+        sayAdditionalGreetings(addgreeting);
+
+        // Close the Scanner when you're done using it
+        input.close();
     }
 }

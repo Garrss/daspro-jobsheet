aec86f4cbdfc9087dcd421fc09b8aa0fc5a2a952
Muhammad Tegar Hibatulloh <143469879+Garrss@users.noreply.github.com>
2023-11-25 23:54:14 +0700

    Jobsheet 13 Assignment

---
+++ CubeStudent21.java
@@ -0,0 +1,14 @@
+public class CubeStudent21 {
+    static int area(int x, int y) {
+        return x * y;
+    }
+
+    static int volume(int x, int y, int z) {
+        return x * y * z;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(area(6, 12));
+        System.out.println(volume(5, 10, 15));
+    }
+}

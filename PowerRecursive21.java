e566da0a5bbe73747418ab92cb7d38f47ce3d720
Muhammad Tegar Hibatulloh <143469879+Garrss@users.noreply.github.com>
2023-12-01 13:43:45 +0700

    Jobsheet 14

--- PowerRecursive21.java
+++ PowerRecursive21.java
@@ -1,13 +1,6 @@
 import java.util.Scanner;
 
 public class PowerRecursive21 {
-    static int calculatePower(int base, int pow) {
-        if (pow == 0)
-            return 1;
-        else
-            return base * calculatePower(base, pow - 1);
-    }
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Input Base Number: ");
@@ -15,5 +8,25 @@ public class PowerRecursive21 {
         System.out.print("Input Power Number: ");
         int Power = input.nextInt();
         System.out.println("Result of " + base + " power " + Power + " = " + calculatePower(base, Power));
+
+        printPower(base, Power);
+    }
+
+    static int calculatePower(int base, int pow) {
+        if (pow == 0)
+            return 1;
+        else
+            return base * calculatePower(base, pow - 1);
+    }
+
+    static void printPower(int base, int pow) {
+        String series = "";
+        for (int i = 0; i < pow; i++) {
+            if (i < pow - 1) {
+                series += "x";
+            }
+        }
+        series += "x1";
+        System.out.println(series + " = " + calculatePower(base, pow));
     }
 }
